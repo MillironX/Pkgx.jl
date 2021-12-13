0fe2a833420dcce2b4d1868b91ebbd63c10a2632
Documenter.jl <documenter@juliadocs.github.io>
2021-12-13 16:09:45 +0000

    build based on 47ac159

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Pkgx","category":"page"},{"location":"#Pkgx","page":"Home","title":"Pkgx","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Pkgx.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Pkgx]","category":"page"}]
+}
